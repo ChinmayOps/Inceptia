52ee13c710e2267c1d861852640b27e96219420f
Chinmay <chinmaykakade28@gmail.com>
2025-05-15 18:03:03 +0530

    changed meta

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
           <div className="space-y-4">
             <h4 className="text-lg font-semibold text-white">Contact Us</h4>
             <ul className="space-y-2">
-              <li><a href="mailto:inceptiahackathon2025@gmail.com" className="text-white/60 hover:text-white transition-colors">inceptiahackathon2025@gmail.com</a></li>
+              <li><a href="mailto:inceptia2025@gmail.com" className="text-white/60 hover:text-white transition-colors">inceptia2025@gmail.com</a></li>
               <li><a href="tel:+91 91521 81521  +91 76202 58694" className="text-white/60 hover:text-white transition-colors">+91 91521 81521 | +91 76202 58694</a></li>
               <li><span className="text-white/60">Pimpri Chinchwad College of Engineering,<br />Ravet, Pune</span></li>
             </ul>
